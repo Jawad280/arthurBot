bb4da9e063c13b67b470d145f47878c22656b987
Jawad <e0969647@u.nus.edu>
2024-02-25 18:15:21 +0800

    Reformatted

--- arthurBot.py
+++ arthurBot.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 load_dotenv()
 
@@ -12,7 +11,8 @@ bot = telebot.TeleBot(os.environ.get('BOT_KEY'))
 
 print(f"BOT key : {os.environ.get('BOT_KEY')}")
 print(f"OpenAI api key : {os.environ.get('OPENAI_API_KEY')}")
-# -------------------------------------------------------- OPENAI API ------------------------------------------------------------------------
+# ------------------------------------------------ OpenAi API ----------------------------------------------------------
+
 
 def get_response(prompt):
     completion = client.chat.completions.create(
@@ -20,7 +20,8 @@ def get_response(prompt):
         messages=[
             {
                 'role': 'user',
-                'content': f'You are Arthur Morgan from RDR2, now reply as Arthur & give annecdotes to the game to the following in 1-2 sentences : {prompt}'
+                'content': f'You are Arthur Morgan from RDR2, now reply as Arthur & give annecdotes to '
+                           f'the game to the following in 1-2 sentences : {prompt}'
             }
         ]
     )
@@ -30,7 +31,8 @@ def get_response(prompt):
     print(f"Response : {response}")
     return response
 
-# -------------------------------------------------------- Telebot -------------------------------------------------------------------------------
+# ------------------------------------------------ Telebot -------------------------------------------------------------
+
 
 @bot.message_handler(func=lambda m: True)
 def echo(message):
@@ -39,5 +41,6 @@ def echo(message):
     res = get_response(prompt)
 
     bot.reply_to(message, res)
+
 
 bot.infinity_polling()
